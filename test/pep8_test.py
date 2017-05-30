075c16651ef4aecdd6014bab56b2ca09c0288800
Volcyy <Johannes.christ@gmail.com>
2017-05-30 19:55:02 +0200

    Add Linter test to make sure commits conform with PEP8.

--- test/pep8_test.py
+++ test/pep8_test.py
@@ -1,22 +1,34 @@
 import os
+import subprocess
 import pycodestyle
 import unittest
 
-class TestCodeFormat(unittest.TestCase):
-    def get_source_file_names(self):
-        """Gets the paths to all .py files found in the `src` directory"""
-        files = []
-        for root, sub_folders, files in os.walk('src'):
-            for file in files:
-                if file.endswith('.py'):
-                    files.append(file)
-        return files
 
+FAIL_MSG = 'Found a total of {} code style errors (and warnings), use ' \
+           '`pylint test/` and `pylint src/` to read about them in detail.'
+
+def get_py_files(dirname: str):
+    """Gets the paths to all .py files found in the specified directory"""
+    files = []
+    for root, sub_folders, directory_name in os.walk(dirname):
+        for file_name in directory_name:
+            # Don't check the virtual environment
+            if directory_name == 'venv':
+                continue
+            elif file_name.endswith('.py'):
+                files.append(file_name)
+    return files
+
+
+class TestCodeFormat(unittest.TestCase):
     def test_pep8(self):
+        """Test that all source files are conform to PEP8."""
         style = pycodestyle.StyleGuide(quiet=True)
-        result = style.check_files(self.get_source_file_names())
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+        result = style.check_files(get_py_files('src') + get_py_files('test'))
+        self.assertEqual(result.total_errors, 0, FAIL_MSG.format(result.total_errors))
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
