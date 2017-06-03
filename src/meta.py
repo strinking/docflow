e9ba0b6280dc02d41c2d4cd9b6257a561791478b
Volcyy <Johannes.christ@gmail.com>
2017-06-03 11:26:01 +0200

    adds invite command

--- src/meta.py
+++ src/meta.py
@@ -8,7 +8,7 @@ SECONDS_IN_A_DAY = 86400
 
 
 class Meta:
-    """Meta Commands with information about the Bot."""
+    """Meta Commands with information about the Bot, as well as an invitation link."""
     def __init__(self, bot: Bot):
         self.bot = bot
 
@@ -25,4 +25,15 @@ class Meta:
         await ctx.send(embed=discord.Embed(
             colour=discord.Colour.blue(),
             description=f'**Uptime**: `{self.bot.uptime}`{unit}'
+        ))
+
+    @commands.command()
+    async def invite(self, ctx):
+        """Gives you an invitation link for the Bot."""
+        link = ('https://discordapp.com/oauth2/authorize?'
+                f'client_id={self.bot.user.id}&scope=bots')
+        await ctx.send(embed=discord.Embed(
+            title='Invitation Link',
+            description=link,
+            colour=discord.Colour.blue()
         ))
