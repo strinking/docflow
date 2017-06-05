2936bfd36be5f2e83cd75de68654d6c5badee417
Volcyy <Johannes.christ@gmail.com>
2017-06-05 15:32:28 +0200

    add stats command

--- src/meta.py
+++ src/meta.py
@@ -15,16 +15,19 @@ class Meta:
     def __unload(self):
         pass
 
-    @commands.command()
-    async def uptime(self, ctx):
-        """Shows the Bot's uptime as well as its starting time."""
+    def get_readable_uptime(self) -> str:
         if self.bot.uptime.total_seconds() > SECONDS_IN_A_DAY:
             unit = 'd'
         else:
             unit = 'h'
+        return str(self.bot.uptime) + unit
+
+    @commands.command()
+    async def uptime(self, ctx):
+        """Shows the Bot's uptime as well as its starting time."""
         await ctx.send(embed=discord.Embed(
             colour=discord.Colour.blue(),
-            description=f'**Uptime**: `{self.bot.uptime}`{unit}'
+            description=f'**Uptime**: `{self.get_readable_uptime()}`'
         ))
 
     @commands.command()
@@ -37,3 +40,22 @@ class Meta:
             description=link,
             colour=discord.Colour.blue()
         ))
+
+    @commands.command()
+    async def stats(self, ctx):
+        """Displays basic stats about the Bot."""
+        stats = discord.Embed()
+        stats.add_field(
+            name='Guilds',
+            value=f'Present in {sum(1 for _ in self.bot.guilds)} Guilds'
+        ).add_field(
+            name='Users',
+            value=(f'**Total**: {sum(1 for _ self.bot.users)}\n'
+                   f'**Unique**: {sum(1 for _ in set(self.bot.users))}')
+        ).add_field(
+            name='Uptime',
+            value=(f'**Online since**: {self.bot.start_time}\n'
+                   f'**Uptime**: {self.get_readable_uptime()}')
+        ).colour = discord.Colour.blue()
+
+        await ctx.send(stats)
