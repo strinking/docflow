8837af0570931f78001f9c91e994ca9ac3761a92
Volcyy <Johannes.christ@gmail.com>
2017-07-23 17:23:44 +0200

    Fix start_urls concatenating because a comma was missing

--- src/scraper/scraper/spiders/cpp_spider.py
+++ src/scraper/scraper/spiders/cpp_spider.py
@@ -4,20 +4,19 @@ import scrapy
 class BjarneSpider(scrapy.Spider):
     name = "cppreference"
     start_urls = [
-        "http://en.cppreference.com/w/c"
+        "http://en.cppreference.com/w/c",
         "http://en.cppreference.com/w/cpp"
     ]
 
     # Parse the index websites listed above
     def parse(self, response):
-        base_url = "http://en.cppreference.com"
-        for url in response.css('a'):
+        for url in response.css('a::attr(href)'):
             # http://en.cppreference.com/w/cpp/language - overview page about language constructs
             if url.endswith('language'):
                 pass
             # http://en.cppreference.com/w/cpp/symbol_index - list of all symbols in the std namespace
             elif url.endswith('symbol_index'):
-                pass
+                yield response.follow(url, callback=self.parse_symbol_index)
             # generic language-related pages, for example http://en.cppreference.com/w/cpp/algorithm
             elif url.startswith('/w/c'):
                 # yield response.follow(base_url + url, callback=)
