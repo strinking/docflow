aeed36025eeff9978a36b3aacec3bc0ac3232e0f
Volcyy <Johannes.christ@gmail.com>
2017-07-23 21:49:58 +0200

    Adds a basic function to parse a `std::` symbol

--- src/scraper/scraper/settings.py
+++ src/scraper/scraper/settings.py
@@ -33,7 +33,7 @@ ROBOTSTXT_OBEY = True
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
@@ -70,11 +70,11 @@ ROBOTSTXT_OBEY = True
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
 #AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
--- src/scraper/scraper/spiders/cpp_spider.py
+++ src/scraper/scraper/spiders/cpp_spider.py
@@ -1,8 +1,11 @@
 import scrapy
+from w3lib.html import remove_tags
 
 
 class BjarneSpider(scrapy.Spider):
     name = "cppreference"
+    # Use google webcache instead?
+    # http://webcache.googleusercontent.com/search?q=cache:en.cppreference.com/w/c/
     start_urls = [
         "http://en.cppreference.com/w/c",
         "http://en.cppreference.com/w/cpp"
@@ -10,7 +13,7 @@ class BjarneSpider(scrapy.Spider):
 
     # Parse the index websites listed above
     def parse(self, response):
-        for url in response.css('a::attr(href)'):
+        for url in response.css('a::attr(href)').extract():
             # http://en.cppreference.com/w/cpp/language - overview page about language constructs
             if url.endswith('language'):
                 pass
@@ -21,3 +24,35 @@ class BjarneSpider(scrapy.Spider):
             elif url.startswith('/w/c'):
                 # yield response.follow(base_url + url, callback=)
                 pass
+
+    # Parses the C++ Symbol Index http://en.cppreference.com/w/cpp/symbol_index
+    def parse_symbol_index(self, response):
+        for url in response.css('p a::attr(href)'):
+            # print(url)
+            yield response.follow(url, callback=self.parse_std_symbol)
+
+    # Parses an C++ symbol defined in the standard namespace, from the index above
+    @staticmethod
+    def parse_std_symbol(response):
+        names = response.css("h1.firstHeading::text").extract()
+        headers = response.css("table.t-dcl-begin tr.t-dsc-header a::text")
+        signatures = response.css("tbody tr.t-dcl span::text").extract()
+        description = response.css("div.mw-content-ltr").xpath("string(p)").extract()
+        parameters = response.css("table.t-par-begin").xpath("string(.//tr)").extract()
+        yield {
+            'names': [
+                "std::" + x.replace(', ', '') for x in names if x != ', '
+            ],
+            'defined_in_header': [
+                x for x in headers if x not in headers
+            ],
+            'sigs': [
+                ''.join(signatures).split(';')
+            ],
+            'desc': [
+                remove_tags(paragraph) for paragraph in description
+            ],
+            'params': [
+                param.replace('\n', '') for param in parameters
+            ],
+        }
