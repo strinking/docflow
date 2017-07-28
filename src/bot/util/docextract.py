a1f3e62d648425de63930b5aa9428021f836ba0d
aeshthetic <eabhijay@protonmail.com>
2017-07-27 21:02:06 -0400

    updated docextract to query json using search and search/return embeds for stubs

--- src/bot/util/docextract.py
+++ src/bot/util/docextract.py
@@ -5,19 +5,50 @@ reference documents and returns them in Embeds, ready to be sent.
 import discord
 import os
 import json
-
+from search import search
 from typing import Optional
 
 
-CPP_SYMBOL_PATH = os.path.join(os.path.abspath(os.path.pardir), 'docflow', 'doc', "cpp_symbols.json")
+CPP_SYMBOL_PATH = os.path.join(os.path.abspath(
+    os.path.pardir), 'doc', "cpp_symbols.json")
+CPP_STUB_PATH = os.path.join(os.path.abspath(os.path.pardir), 'doc', 'stub.json') 
+
+def cpp_stub(query: str) -> discord.Embed:
+    with open(CPP_STUB_PATH, 'r') as f:
+        data = json.load(f)
+    
+    names = [obj["name"] for obj in data]
+    search_result = search(names, query)
+    for stub_obj in data:
+        if any(n == search_result for n in stub_obj["name"]):
+            stub = stub_obj
+            break
+        else:
+            return None
+    return discord.Embed(
+        title=stub['name'],
+        description=stub['items_raw'][0],
+        colour=discord.Colour.dark_blue()
+    ).set_footer(
+        text="Data from cppreference.com, licensed under CC-BY-SA and GFDL."
+    ).add_field(
+        name="Defined in Header(s)",
+        value="```cpp\n" + ';\n'.join(stub['defined_in_header']) + "```"
+    ).add_field(
+        name="Link",
+        value=stub["link"]
+    )
 
 
 def cpp_symbol(name: str) -> Optional[discord.Embed]:
     with open(CPP_SYMBOL_PATH, 'r') as f:
         data = json.load(f)
 
+    names = [obj["name"] for obj in data]
+    search_result = search(names, name)
+
     for symbol_obj in data:
-        if any(n == name for n in symbol_obj['names']):
+        if any(n == search_result for n in symbol_obj['names']):
             symbol = symbol_obj
             break
     else:
