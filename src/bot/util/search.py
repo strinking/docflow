e208b49f3d36972c1af4f99d8a809607283c379a
aeshthetic <eabhijay@protonmail.com>
2017-07-28 03:15:02 -0400

    updated search util to use a custom levenshtein function and ratio function

--- src/bot/util/search.py
+++ src/bot/util/search.py
@@ -1,6 +1,36 @@
-import Levenshtein
+import numpy as np
+
+def levenshtein(src, tgt):
+    if len(src) < len(tgt):
+        return levenshtein(tgt, src)
+    if len(tgt) == 0:
+        return len(src)
+    src = np.array(tuple(src))
+    tgt = np.array(tuple(tgt))
+
+    prev_row = np.arange(tgt.size + 1)
+    for s in src:
+        curr_row = prev_row + 1
+        curr_row[1:] = np.minimum(
+                curr_row[1:],
+                np.add(prev_row[:-1], tgt != s))
+        curr_row[1:] = np.minimum(
+                curr_row[1:],
+                curr_row[0:-1] + 1)
+        prev_row = curr_row
+    return prev_row[-1]
+
+def ratio(s1, s2):
+    if len(s1) > len(s2):
+        return levenshtein(s1, s2) / len(s1)
+    else:
+        return levenshtein(s1, s2) / len(s2)
+
 
 def search(items, query):
-    results = [(item, Levenshtein.ratio(item, query)) for item in items]
-    sorted_results = sorted(results, key=lambda x: x[1], reverse=True)
+    results = [(item, ratio(item, query)) for item in items]
+    print(results)
+    sorted_results = sorted(results, key=lambda x: x[1], reverse=False)
+    print(sorted_results)
     return sorted_results[0][0]
+
