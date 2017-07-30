1671c7b43331749860134e91c6f680fc27e4e136
aeshthetic <eabhijay@protonmail.com>
2017-07-30 16:44:09 -0400

    fixed pylint issues with search.py

--- src/bot/extract/search.py
+++ src/bot/extract/search.py
@@ -1,10 +1,16 @@
+"""
+Utility module for finding the levenshtein 
+distance/ratio between two strings and using it
+to search a list for a query
+"""
 import numpy as np
 
 
 def levenshtein(src, tgt):
+    """returns the levenshtein distance between src and tgt"""
     if len(src) < len(tgt):
         return levenshtein(tgt, src)
-    if len(tgt) == 0:
+    if not tgt:
         return len(src)
     src = np.array(tuple(src))
     tgt = np.array(tuple(tgt))
@@ -23,13 +29,18 @@ def levenshtein(src, tgt):
         prev_row = curr_row
     return prev_row[-1]
 
-
-def ratio(a, b):  # pylint: disable=invalid-name
-    if len(a) > len(b):
-        return levenshtein(a, b) / len(a)
-    return levenshtein(a, b) / len(b)
+def ratio(str1, str2):
+    """uses the levenshtein distance between str1 and str2 to find the levenshtein ratio"""
+    if len(str1) > len(str2):
+        return levenshtein(str1, str2) / len(str1)
+    else:
+        return levenshtein(str1, str2) / len(str2)
 
 
 def search(items, query):
+    """
+    compares a query against every item in a list and finds 
+    the lowest levenshtein ratio in order to determine the closest match
+    """
     results = [(item, ratio(item, query)) for item in items]
     return min(results, key=lambda r: r[1])[0]
