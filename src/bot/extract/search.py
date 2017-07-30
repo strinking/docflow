4043afaa4b9d11b1a365e3a4fd3b3b3b5b5de0b2
Volcyy <Johannes.christ@gmail.com>
2017-07-30 22:55:25 +0200

    Actually fix pylint issues and some grammar stuff.

--- src/bot/extract/search.py
+++ src/bot/extract/search.py
@@ -1,46 +1,50 @@
 """
-Utility module for finding the levenshtein 
-distance/ratio between two strings and using it
-to search a list for a query
+Utility module for finding the Levenshtein
+distance/ratio between two strings and using
+it to search a list for a query
 """
+
 import numpy as np
 
 
-def levenshtein(src, tgt):
-    """returns the levenshtein distance between src and tgt"""
-    if len(src) < len(tgt):
-        return levenshtein(tgt, src)
-    if not tgt:
-        return len(src)
-    src = np.array(tuple(src))
-    tgt = np.array(tuple(tgt))
+def levenshtein(source, target):
+    """Returns the Levenshtein distance between source and target"""
 
-    prev_row = np.arange(tgt.size + 1)
-    for s in src:
+    if len(source) < len(target):
+        return levenshtein(target, source)
+    if not target:
+        return len(source)
+    source = np.array(tuple(source))
+    target = np.array(tuple(target))
+
+    prev_row = np.arange(target.size + 1)
+    for src in source:
         curr_row = prev_row + 1
-        curr_row[1:] = np.minimum(
+        curr_row[1:] = np.minimum(  # pylint: disable=no-member
             curr_row[1:],
-            np.add(prev_row[:-1], tgt != s)
+            np.add(prev_row[:-1], target != src)
         )
-        curr_row[1:] = np.minimum(
+        curr_row[1:] = np.minimum(  # pylint: disable=no-member
             curr_row[1:],
             curr_row[0:-1] + 1
         )
         prev_row = curr_row
     return prev_row[-1]
 
-def ratio(str1, str2):
-    """uses the levenshtein distance between str1 and str2 to find the levenshtein ratio"""
-    if len(str1) > len(str2):
-        return levenshtein(str1, str2) / len(str1)
-    else:
-        return levenshtein(str1, str2) / len(str2)
+
+def ratio(a, b):  # pylint: disable=invalid-name
+    """Uses the Levenshtein distance between a and b to find the Levenshtein ratio"""
+
+    if len(a) > len(b):
+        return levenshtein(a, b) / len(a)
+    return levenshtein(a, b) / len(b)
 
 
 def search(items, query):
     """
-    compares a query against every item in a list and finds 
-    the lowest levenshtein ratio in order to determine the closest match
+    Compares a query against every item in a list and finds
+    the lowest Levenshtein ratio in order to determine the closest match
     """
+
     results = [(item, ratio(item, query)) for item in items]
     return min(results, key=lambda r: r[1])[0]
