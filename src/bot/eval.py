a498233cb298e710e2531813a2bbd6e28c432a36
Volcyy <Johannes.christ@gmail.com>
2017-07-30 23:29:23 +0200

    Fix SyntaxError, minor indentation fix.

--- src/bot/eval.py
+++ src/bot/eval.py
@@ -13,18 +13,18 @@ from util import coliru  # pylint: disable=no-name-in-module
 LANGUAGE_IMAGES = {
     'c':     'https://cdn.discordapp.com/emojis/232956938965614592.png',
     'cpp':   ('http://logos-vector.com/images/logo/xxl/'
-             '1/3/7/137285/C__7d201_450x450.png'),
+              '1/3/7/137285/C__7d201_450x450.png'),
     'py':    ('https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/'
-             'Python-logo-notext.svg/1024px-Python-logo-notext.svg.png'),
+              'Python-logo-notext.svg/1024px-Python-logo-notext.svg.png'),
     'sh':    ('https://openforums.files.wordpress.com/'
-             '2013/06/terminal-icon-512x512.png'),
+              '2013/06/terminal-icon-512x512.png'),
     'ruby':  ('https://cdn.codementor.io/assets/topic/category_header/'
-             'ruby-on-rails-bc9ab2af8d92eb4e7eb3211d548a09ad.png'),
+              'ruby-on-rails-bc9ab2af8d92eb4e7eb3211d548a09ad.png'),
     'lua':   ('https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/'
-             'Lua-Logo.svg/947px-Lua-Logo.svg.png')
+              'Lua-Logo.svg/947px-Lua-Logo.svg.png'),
     'perl':  ('https://engineering.purdue.edu/people/joseph.r.kline.1/talks/'
-             'ppw/images_to_use/perl-onion-logo.png')
-    'perl6': ('https://hbfs.files.wordpress.com/2009/11/camelia-logo.png')
+              'ppw/images_to_use/perl-onion-logo.png'),
+    'perl6': 'https://hbfs.files.wordpress.com/2009/11/camelia-logo.png'
 }
 
 LANGUAGE_NAMES = {
