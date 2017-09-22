384c42e8b9b17dc14c230dbbba02e1a8be98f1a8
Volcyy <Johannes.christ@gmail.com>
2017-09-22 11:42:48 +0200

    Move downloaded docs to data directory.

--- scripts/pull_docs.sh
+++ scripts/pull_docs.sh
@@ -1,8 +1,20 @@
+readonly languages=(
+    'c'
+    'cpp'
+    'git'
+    'python@3.6'
+)
+
+start_dir="$PWD"
+
 cd /tmp
 git clone https://github.com/Thibaut/devdocs
 cd devdocs
-command -v bundler >/dev/null 2>&1 || {
-    gem install bundler
-}
+
+command -v bundler >/dev/null 2>&1 || gem install bundler
 bundle install || echo "Failed to install dependencies." || exit 1
-bundle exec thor docs:download c cpp git python@3.6
+
+bundle exec thor docs:download "${languages[@]}"
+mv public/docs/* "$start_dir/data/"
+
+cd "$start_dir"
