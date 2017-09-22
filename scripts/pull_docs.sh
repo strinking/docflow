17ff85516a040a174e442f2a3e55d89191e0b00a
Volcyy <Johannes.christ@gmail.com>
2017-09-22 11:20:30 +0200

    Add a pull docs script.

---
+++ scripts/pull_docs.sh
@@ -0,0 +1,8 @@
+cd /tmp
+git clone https://github.com/Thibaut/devdocs
+cd devdocs
+command -v bundler >/dev/null 2>&1 || {
+    gem install bundler
+}
+bundle install || echo "Failed to install dependencies." || exit 1
+bundle exec thor docs:download c cpp git python@3.6
