03a3bc7ea577c0a6cae803cfae9795e576ccfb2f
Volcyy <Johannes.christ@gmail.com>
2017-09-23 11:57:56 +0200

    Fix a dirname-related issue.

--- scripts/convert.py
+++ scripts/convert.py
@@ -7,10 +7,12 @@ import glob
 import os
 
 from markdownify import markdownify
+from tqdm import tqdm
 
 
 if __name__ == '__main__':
-    for fname in glob.iglob("data/**/*.html"):
+    for fname in tqdm(glob.glob("data/**/*.html", recursive=True)):
+        if "python" in "fname": print(fname)
         with open(fname) as f:
             html = f.read()
         with open(fname.split('.')[0] + '.md', 'w+') as f:
--- scripts/pull_docs.sh
+++ scripts/pull_docs.sh
@@ -8,13 +8,14 @@ readonly languages=(
 start_dir="$PWD"
 
 cd /tmp
-cd devdocs && git pull || git clone https://github.com/Thibaut/devdocs && cd devdocs
+cd devdocs && git pull || git clone https://github.com/Thibaut/devdocs && cd devdocs
 
 command -v bundler >/dev/null 2>&1 || gem install bundler
 bundle install || echo "Failed to install dependencies." || exit 1
 
 bundle exec thor docs:download "${languages[@]}"
-mv -f public/docs/* "$start_dir/data/"
+rsync -a public/docs/* "$start_dir/data/"
+mv "$start_dir/data/python\~3.6" "$start_dir/data/python"
 
 cd "$start_dir"
 echo "Got documentation. Converting to Markdown..."
